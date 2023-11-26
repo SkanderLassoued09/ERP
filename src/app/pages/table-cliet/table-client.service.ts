03ac695e70dffe15735565cf63a2e0b8137a833f
@lassoued.skanderr <lassoued.skanderr@gmail.com>
2023-11-26 21:31:14 +0100

    deploy version

--- src/app/pages/table-cliet/table-client.service.ts
+++ src/app/pages/table-cliet/table-client.service.ts
@@ -10,10 +10,6 @@ export class TableClientService {
 
   addClient(userData: Client, type: string) {
     return gql`
-
-
-
-
        mutation {createClient(compClient:"${type}",createClientInput:{firstName:"${userData.firstName}" lastName:"${userData.lastName}" email:"${userData.email}" phone:"${userData.phone}" address:"test" companyName:"${userData.companyName}" region:"${userData.region}" codePostal:"${userData.codePostal}" tva:"${userData.tva}"
        etat:"${userData.etat}" fax:"${userData.fax}" ibanRib:"${userData.ibanRib}" nRegisterCommerce:"${userData.nRegisterCommerce}" swiftBic:"${userData.swiftBic}" nattestation:"${userData.nattestation}" codeFiscal:"${userData.codeFiscal}" conPayment:"${userData.conPayment}" website:"${userData.website}" techContact:"${userData.techContact}"}) {firstName}}
     `;
