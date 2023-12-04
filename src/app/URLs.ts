f906f064249ac51b1b38b37f61b6f5a778177a7b
@lassoued.skanderr <lassoued.skanderr@gmail.com>
2023-12-04 20:28:15 +0100

    url

--- src/app/URLs.ts
+++ src/app/URLs.ts
@@ -1,4 +1,6 @@
 export const URL = {
-  URL: `http://localhost:3000/graphql`,
-  SOCKET: "http://localhost:3000",
+  URL: `http://http://102.211.209.40/:3000/graphql`,
+  SOCKET: "http://http://102.211.209.40/:3000",
 };
+
+//
